ecb659955bf0e6c39df3c739c7e663b74f9a5739
MykolaZaichenko6261 <tiger62613@gmail.com>
2022-07-01 13:44:43 +0300

    Create Subject

---
+++ backend/lesson5/lesson/students/models.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from re import T
+from tabnanny import verbose
+from django.db import models
+
+class Subject(models.Model):
+    name = models.CharField(verbose_name="Название",
+                             max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args,**kwargs)
+    
+    class Meta:
+        verbose_name ="Предмет"
+        verbose_name_plural = "Предметы"
+    
+class StudentGroup(models.Model):
+    name = models.CharField(verbose_name="Имя",
+                             max_length=100)
+    students_number = models.DecimalField(verbose_name="Количество студентов",
+                                        max_digits=4,
+                                        decimal_places=0,
+                                        null=True,
+                                        blank=True)
+    subject = models.ForeignKey(Subject,verbose_name="Предмет",
+                                on_delete=models.SET_NULL,
+                                null=True,
+                                blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args,**kwargs)
+    
+    class Meta:
+        verbose_name ="Группа студентов"
+        verbose_name_plural = "Группы студентов"
+
+class Student(models.Model):
+
+    SURNAME_CHOICES=(
+        ('Petrenko','Петренко'),
+        ('Ivanenko','Иваненоко'),
+        ('Sidorov','Сидоров'),
+        ('Maximenko','Максименко'),
+        ('Mykhailenko','Михайленко')
+    )
+    name = models.CharField(verbose_name="Имя",
+                             max_length=100,
+                             blank=True,
+                             null=True)
+    surname= models.CharField(verbose_name="Фамилия",
+                            max_length=100,
+                            null=True,
+                            blank=True,
+                            choices=SURNAME_CHOICES)
+    birthday = models.DateField(verbose_name="День рождения",
+                                null=True,
+                                blank=True)
+    group = models.ForeignKey(StudentGroup,verbose_name="Группа",
+                                on_delete=models.SET_NULL,
+                                null=True,
+                                blank=True )
+    
+
+    def __str__(self) -> str:
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args,**kwargs)
+    
+    class Meta:
+        verbose_name ="Студент"
+        verbose_name_plural = "Студеты"
---
+++ backend/lesson5/lesson/students/views.py
@@ -0,0 +1,36 @@
+from socket import gaierror
+from sqlite3 import DatabaseError
+from unicodedata import name
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.views import View
+from students.models import Subject,StudentGroup,Student
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+class SubjectView(View):
+    def get(self,request):
+        subjects=Subject.objects.all()
+        
+        return HttpResponse(subjects)
+
+
+class StudentLearnSubject(View):#Решить вопрос с id
+    def get(self, request,id):
+        
+        try:
+            pr=id[4:len(id)-1]
+            subjects=Subject.objects.get(id=pr)
+            group_name=StudentGroup.objects.filter(subject=subjects)
+            student_names=[] 
+            for groups in group_name:
+                stud=Student.objects.filter(group=groups)
+                for student in stud:
+                    student_names.append(student)
+                
+            return HttpResponse(student_names)
+        
+            
+        except ObjectDoesNotExist:
+            return HttpResponse("Такой id не существует")
+        
